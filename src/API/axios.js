a42791befa80bd7c6567fe62c3379ce83654f809
pr644 <bhagyashreelphabit@gmail.com>
2026-01-16 19:32:41 +0530

    first commit

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: import.meta.env.VITE_BASE_URL,
+ baseURL: import.meta.env.VITE_BASE_URL,  
+
+ headers:{
+    'Content-Type':'application/json'
+ },
 });
 
 
