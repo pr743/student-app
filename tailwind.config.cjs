2ff80d69ec8dcd4bc94903e56352b6f606e9179e
pr644 <bhagyashreelphabit@gmail.com>
2026-01-16 17:40:38 +0530

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', 
+  ],
   theme: {
     extend: {},
   },
